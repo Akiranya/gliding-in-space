7f9aa3e333b77f46b5b5524eb3a3512e632e73ac
Andy <u6545267@anu.edu.au>
2019-09-14 02:03:01 +1000

    it now roughly works ...

--- Assignment_1_template/Sources/Student_Packages/vehicle_message_type.ads
+++ Assignment_1_template/Sources/Student_Packages/vehicle_message_type.ads
@@ -9,10 +9,11 @@ package Vehicle_Message_Type is
    -- Replace this record definition by what your vehicles need to communicate.
 
    type Inter_Vehicle_Messages is record
-      No : Positive;
+      Source_ID : Positive; -- who found the globe
+      Forwarder_ID : Positive; -- who sent the message
       Globe_Loc : Swarm_Structures_Base.Positions;
       Globe_Vel : Swarm_Structures_Base.Velocities;
-      Towards_Globe : Boolean;
+      Charging : Boolean;
    end record;
 
 end Vehicle_Message_Type;
--- Assignment_1_template/Sources/Student_Packages/vehicle_task_type.adb
+++ Assignment_1_template/Sources/Student_Packages/vehicle_task_type.adb
@@ -9,6 +9,8 @@ with Vehicle_Interface;          use Vehicle_Interface;
 with Vehicle_Message_Type;       use Vehicle_Message_Type;
 --  with Swarm_Structures;           use Swarm_Structures;
 with Swarm_Structures_Base; use Swarm_Structures_Base;
+with Ada.Numerics; use Ada.Numerics;
+
 --  with Ada.Text_IO; use Ada.Text_IO;
 --  with Central_Coordinator; use Central_Coordinator;
 
@@ -19,18 +21,35 @@ package body Vehicle_Task_Type is
    task body Vehicle_Task is
 
       Vehicle_No : Positive;
---        Recent_Globe_Info : Energy_Globe;
-      type Tick is mod Natural'Last;
-      Time : Tick; -- time for drawing circle
-
+      Recent_Messages : Inter_Vehicle_Messages;
+      Local_Charging : Boolean := False;
+      Time : Real := 0.0; -- time for drawing circle
+      Radius : constant Real := 0.25; -- orbit radius
+      Orbit : Vector_3D;
 
       -- helper function: get a element from array
       -- grabs the first element in this array from the time being.
       function Grab_A_Globe (Globes : Energy_Globes) return Energy_Globe is (Globes (1));
       function Has_Energy_Nearby (Globes : Energy_Globes) return Boolean is (Globes'Length > 0);
 
-      -- helper function: group all ships
-      function From_Even return Boolean is (Vehicle_No mod 2 = 0);
+      procedure Update_Charging (State : Boolean) is
+      begin
+         Local_Charging := State;
+         Recent_Messages.Charging := State;
+      end;
+
+      procedure Orbiting (Throttle : Real; Tick : Real) is
+      begin
+         Orbit := (Real_Elementary_Functions.Cos (Time),
+                   Real_Elementary_Functions.Sin (Time),
+                   0.0);
+         Orbit := Orbit * Radius; -- (cos(Time) + sin(Time)) * Radius
+         Orbit := Orbit + Recent_Messages.Globe_Loc; -- adjusts orbiting origin
+         Orbit := Orbit + Recent_Messages.Globe_Vel; -- predicits oribiting origin
+         Time := Time + Pi / Tick; -- increment Time for next calculation
+         Set_Destination (Orbit);
+         Set_Throttle (Throttle);
+      end Orbiting;
 
       procedure Report (Info : String) is
       begin
@@ -46,10 +65,13 @@ package body Vehicle_Task_Type is
       accept Identify (Set_Vehicle_No : Positive; Local_Task_Id : out Task_Id) do
          Vehicle_No     := Set_Vehicle_No;
          Local_Task_Id  := Current_Task;
-         Time := 0;
-      end Identify;
 
-      Report ("spawned.");
+         Recent_Messages := (Source_ID => 999, -- non-existent no, placeholder
+                             Forwarder_ID => Vehicle_No,
+                             Globe_Loc => Zero_Vector_3D,
+                             Globe_Vel => Zero_Vector_3D,
+                             Charging => False);
+      end Identify;
 
       -- Replace the rest of this task with your own code.
       -- Maybe synchronizing on an external event clock like "Wait_For_Next_Physics_Update",
@@ -69,51 +91,75 @@ package body Vehicle_Task_Type is
 
             -- Your vehicle should respond to the world here: sense, listen, talk, act?
 
---              Set_Throttle (1.0);
---              Set_Destination (Zero_Vector_3D);
---              Target : constant Vector_3D := (5.0, 5.0, 5.0); -- the way how to create a vector.
-
             -- send message if found the globe(s).
             if Has_Energy_Nearby (Energy_Globes_Around) then
                declare
                   Lucky_Globe : constant Energy_Globe := Grab_A_Globe (Energy_Globes_Around);
-                  Lucky_Info : constant Inter_Vehicle_Messages := (No => Vehicle_No,
+                  Lucky_Info : constant Inter_Vehicle_Messages := (Source_ID => Vehicle_No,
+                                                                   Forwarder_ID => Vehicle_No,
                                                                    Globe_Loc => Lucky_Globe.Position,
                                                                    Globe_Vel => Lucky_Globe.Velocity,
-                                                                   Towards_Globe => False);
+                                                                   Charging => False);
                begin
-                  Send (Message => Lucky_Info);
+                  Recent_Messages := Lucky_Info;
+                  Send (Recent_Messages);
+--                    Report ("globbe nearby.");
                end;
             end if;
 
-            -- try to receive message.
-            -- if this globe receives a message,
+            -- try to receive message:
+            -- if this ship receives a message,
             -- it should spread this message to its nearby globes.
             if Messages_Waiting then
                declare
+--                    Distance : Real; -- distance between this ship and globe found
                   Incomming_Message : Inter_Vehicle_Messages;
-                  Globe_Loc : Positions; -- helper local var
-                  Globe_Vel : Velocities; -- helper local var
-                  Distance : Real; -- distance between this ship and globe found
                begin
-                  Receive (Incomming_Message); -- retrives the message.
-                  Globe_Loc := Incomming_Message.Globe_Loc;
-                  Globe_Vel := Incomming_Message.Globe_Vel;
---                    Put_Line (Vehicle_No'Image & " got message! " & Image (Globe_Vel));
-
-                  -- the distance between this ship and the globe
-                  Distance := Vectors_3D."abs" (Globe_Loc - Position);
---                    Report ("Distance: " & Distance'Image);
-                  if Distance > 0.4 and From_Even then
---                       Report ("In control.");
-                     -- let the ship follow the globe:
-                     Set_Throttle (1.0);
-                     Set_Destination (Globe_Loc);
+                  Receive (Incomming_Message);
+                  -- only updates info if the message is from new source, or
+                  -- some ship's going to charge (Charging = True)
+--                    if not (Incomming_Message.Source_ID = Vehicle_No)
+--                      and then not (Incomming_Message.Forwarder_ID = Vehicle_No)
+                  if not (Incomming_Message.Forwarder_ID = Vehicle_No)
+                  then
+                     Recent_Messages := Incomming_Message;
+--                       Report ("incomming new message. Source: " & Recent_Messages.Source_ID'Image);
                   end if;
 
                   -- spread incomming message to nearby ships:
-                  Send (Incomming_Message);
+                  Send (Recent_Messages);
                end;
+            end if;
+
+            -- if this ship is not going to charge, then
+            -- let it orbit around the globe.
+            if not Local_Charging
+            then
+               Orbiting (Throttle => 0.5,
+                         Tick     => 100.0);
+            end if;
+
+            -- try to charge:
+            if Current_Charge < 0.75 and then not Recent_Messages.Charging then
+               Update_Charging (True); -- tells other ships i'm going to charge.
+               Send (Recent_Messages);
+
+               Set_Destination (Recent_Messages.Globe_Loc);
+               Set_Throttle (1.0); -- as faster as possible
+               Report ("charging!");
+            end if;
+
+            -- after recharging, go back to orbit by using *local* globe info
+            if Recent_Messages.Charging and then Current_Charge >= 0.75 then
+               Update_Charging (False); -- tells other ships I've finished charging.
+
+               -- go back to orbit
+               Orbiting (Throttle => 1.0,
+                         Tick     => 100.0);
+               Report ("back to orbit.");
+
+               delay 0.2; -- wait until the ship is on orbit.
+               Send (Recent_Messages); -- !!! no guarantee to be received. !!!
             end if;
 
          end loop Outer_task_loop;
@@ -126,3 +172,9 @@ package body Vehicle_Task_Type is
    end Vehicle_Task;
 
 end Vehicle_Task_Type;
+
+--              if abs (Recent_Messages.Globe_Loc - Position) > 0.4 then
+--                 Report ("In control.");
+--                 Set_Throttle (1.0);
+--                 Set_Destination (Recent_Messages.Globe_Loc);
+--              end if;
